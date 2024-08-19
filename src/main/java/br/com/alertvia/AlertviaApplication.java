fc67792cab640af02b7ffe56183ca824fd4640a9
Gabriela Oliveira <ol.gabriela06@gmail.com>
2024-08-19 09:05:00 -0300

    primeira versão do projeto

---
+++ src/main/java/br/com/alertvia/AlertviaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alertvia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alertvia/AlertviaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alertvia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
