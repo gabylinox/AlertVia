2621ed7f9b7e8b568f073949057f7977d2efaccc
Vitor Ramos <vitor.echoshow@gmail.com>
2024-08-26 15:53:10 -0300

    Atualizando teste git geral

---
+++ src/main/java/br/com/alertvia/model/model/Cadastro.java
@@ -0,0 +1,27 @@
+package br.com.alertvia.model.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cadastro{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String bairro;
+    private String rua;
+    private String complemento;
+    private String tipo_de_ocorrencia;
+    private String foto_ou_anexo;
+    private String urgencia;
+}
