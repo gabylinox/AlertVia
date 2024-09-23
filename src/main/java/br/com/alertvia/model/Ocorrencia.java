9bc5f36fd6a655f589a3246457cb16cbefbbfb49
Vitor Ramos <vitor.echoshow@gmail.com>
2024-09-23 10:37:32 -0300

    Adicionado Listagem

--- src/main/java/br/com/alertvia/model/Ocorrencia.java
+++ src/main/java/br/com/alertvia/model/Ocorrencia.java
@@ -26,6 +26,8 @@ public class Ocorrencia {
     @OneToOne(cascade = CascadeType.ALL)
     private Endereco endereco;
 
+    private String image;
+
 
     public void setId(Long id) {
         this.id = id;
